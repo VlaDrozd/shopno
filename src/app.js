26c3d9d4cadd7b2e2c8298425133341935769e1b
VlaDrozd <vladrozd2000@gmail.com>
2020-02-22 13:20:02 +0300

    feat: add all layaut

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
 import './scss/base.scss'
 import './js/script'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+ymaps.ready(init);
+function init() {
+  let myMap = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 7
+  });
+}
